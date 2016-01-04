c17eebaf40bb16cbdc13221542ccadccc3083831
AlfVarRom <varela_7@hotmail.es>
2016-01-04 11:26:29 +0100

    una ; que se me escapa del app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     methodOverride = require("method-override"),
-    mongoose = require('mongoose'),
+    mongoose = require('mongoose');
     //path = require('path');
 
 // Connection to DB
